874b0358b5cb10cb094779c4e13fe9dac5a39d06
ilnaz <ailnaz94-dev@ya.ru>
2023-11-11 14:53:05 +0400

    Init project

---
+++ src/test/java/ru/springshop/servicediscovery/ServiceDiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.springshop.servicediscovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceDiscoveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
